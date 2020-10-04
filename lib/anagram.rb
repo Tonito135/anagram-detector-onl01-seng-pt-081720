642914203ad93fce290cfb3307c8facc44e5e0a2
Anthony Hernandez <anthonyh2020@gmail.com>
2020-10-04 23:32:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,11 @@ class Anagram
   end
   
   def match(array_possible_anagrams)
-    array_possible_anagrams
+    array_possible_anagrams.select do |words|
+      array_possible_anagrams.split("")= word.split("")
+    end
+  end
+  
     
   
   
